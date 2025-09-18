aa4b7e2c6cf522ca45d8bd25da9430f1e7c4beb2
unknown <jason.422024004@civitas.ukrida.ac.id>
2025-09-18 16:13:43 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    public function get_product(){
+        //get all products
+        $sql = $this->select("products.*", "category_product.product_category_name as product_category_name", "supplier.supplier_name as supplier_name")
+                    ->join('category_product', 'category_product.id', '=', 'products.product_category_id')
+                    ->join('supplier', 'supplier.id', '=', 'products.supplier_id');
+                    
+        return $sql;
+    }
+}
